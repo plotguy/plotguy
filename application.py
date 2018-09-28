944abff8141c5ce634fdc40f614e72186256617e
naveenkiyer <nki2kd@virginia.edu>
2018-09-27 20:10:32 -0700

    more changes

--- application.py
+++ application.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/purchase')
+@application.route('/purchase')
 def purchase():
     return render_template('purchase.html')
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    app.debug = True
-    app.run()
+    application.debug = True
+    application.run()
