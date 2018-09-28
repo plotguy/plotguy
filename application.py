d1e76bb524bc7f940708dff1d8bb7719cbfc3731
naveenkiyer <nki2kd@virginia.edu>
2018-09-27 19:50:38 -0700

    forgot to add these

--- application.py
+++ application.py
@@ -1,32 +1,14 @@
 from flask import Flask
+from flask import render_template
 
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
+app = Flask(__name__)
 
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>Plotguy</title> </head>\n<body>'''
-instructions = '''
-    <h1> fuk u akshay</h1>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+@app.route('/purchase')
+def purchase():
+    return render_template('purchase.html')
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
-
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
-
-# run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    app.debug = True
+    app.run()
